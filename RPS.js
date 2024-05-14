e2d586a32b655e93a8e95c20f3c2f6b848130d26
nvandy23 <nvandy2373@gmail.com>
2024-05-14 13:33:05 -0400

    randomize computer  choices once player choice is selected. Working.

--- RPS.js
+++ RPS.js
@@ -1,8 +1,12 @@
 const yc=document.querySelector(".Your_choices")
+const cc =document.querySelector(".com_choice")
+const array = ["Rock","Paper","Scissors"]
 
-
-yc.addEventListener('click',() =>{
-    console.log("nice")
+yc.addEventListener('click',(evt) =>{
+    console.log(evt.target.textContent)
+    const randomIndex = Math.floor(Math.random() * array.length);
+    cc.textContent = array[randomIndex];
 })
+
 
 
